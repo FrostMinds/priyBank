5a276888c642a6ac78479e651af14e66266a7161
FrostMind <Priimak98@Gmail.com>
2024-05-31 23:45:03 +1000

    Master branch

---
+++ priyBank/src/main/java/com/bank/priyBank/PriyBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.priyBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriyBankApplication.class, args);
+	}
+
+}
---
+++ priyBank/src/test/java/com/bank/priyBank/PriyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.priyBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
